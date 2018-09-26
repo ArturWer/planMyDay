c849376b00efc21527356f4477b1df768a6011e5
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-09-26 08:28:50 +0300

    Added clearNewTask function and event code

--- scripts/planMyDay.js
+++ scripts/planMyDay.js
@@ -1,15 +1,26 @@
-let $addNewTask = $('#addNewTask');
+let $addNewTask = $('#addNewTask'),
+	$clearButton = $('#clearButton');
 
 //Stop sending form
 $('button').on('click', function(event){
 	event.preventDefault();
 });
+
+$clearButton.on('click', function(event){
+	clearNewTask();
+});
 //getting new user's task
 $addNewTask.on('click', function(event){
 	let newTaskValue = document.getElementById('newTaskName').value;
+	clearNewTask();
 	if ($('[class="empty"]')) {
 		$('#userHistoryTask').html(`<option>${newTaskValue}</option>`);
 	}
 
 	console.log(newTaskValue);
 });
+
+//clear newTask text element
+function clearNewTask(){
+	document.getElementById('newTaskName').value="";
+};
