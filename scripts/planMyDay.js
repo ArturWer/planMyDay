bc42bf1f19a683645e6912c44533b4a178da9925
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-11-10 07:54:51 +0200

    added function showTask

--- scripts/planMyDay.js
+++ scripts/planMyDay.js
@@ -1,11 +1,26 @@
 let btnAddTask = document.querySelector(".addTask"),
 	btnClear = document.querySelector(".clearButton"),
 	newTasksField = document.querySelector(".newTasks"),
+	dayShow = document.querySelector(".dayShow");
 	day = [];
 
 function Task (name, hours, minutes){
 	this.name = name,
 	this.duration = [hours, minutes];
+}
+function clearTask (){
+	document.getElementById('newTaskName').value = "";
+	document.getElementById("hours").value = 0;
+	document.querySelector(".minutes").value = 0;
+}
+function showTasks(){
+	dayShow.innerHTML = "";
+	for (let i = day.length - 1; i >= 0; i--) {
+		let el = document.createElement("p");
+		let text = document.createTextNode(`${day[i].name} ${day[i].duration[0]}h ${day[i].duration[1]} minutes`);
+		el.appendChild(text);
+		dayShow.appendChild(el);
+	}
 }
 
 btnAddTask.addEventListener('click', function(){
@@ -15,11 +30,11 @@ btnAddTask.addEventListener('click', function(){
 	let newTaskObj = new Task(name, hours, minutes);
 	day.push(newTaskObj);
 	console.log(day);
+	clearTask();
+	showTasks();
 }, false);
 
-btnClear.addEventListener('click', function(){
-	document.getElementById('newTaskName').value = "";
-}, false);
+btnClear.addEventListener('click', clearTask, false);
 
 
 
