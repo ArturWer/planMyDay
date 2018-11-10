b4360a0f294a446f6848c933d3fe6a94e768bd5f
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-11-10 07:27:11 +0200

    Rewrite code. Realized creating new Task object constructor and push it into day array

--- scripts/planMyDay.js
+++ scripts/planMyDay.js
@@ -1,49 +1,25 @@
 let btnAddTask = document.querySelector(".addTask"),
 	btnClear = document.querySelector(".clearButton"),
-	newTasksField = document.querySelector(".newTasks");
+	newTasksField = document.querySelector(".newTasks"),
+	day = [];
 
-function Task (name, duration){
-	name = this.name,
-	duration = this.duration;
+function Task (name, hours, minutes){
+	this.name = name,
+	this.duration = [hours, minutes];
 }
 
 btnAddTask.addEventListener('click', function(){
-	let name = document.getElementById('newTaskName').textContent;
+	let name = document.getElementById('newTaskName').value,
+		hours = document.getElementById("hours").value,
+		minutes = document.querySelector(".minutes").value;
+	let newTaskObj = new Task(name, hours, minutes);
+	day.push(newTaskObj);
+	console.log(day);
 }, false);
 
 btnClear.addEventListener('click', function(){
-	document.getElementById('newTaskName').textContent = "";
+	document.getElementById('newTaskName').value = "";
 }, false);
 
-//getting new user's task
-$addNewTask.on('click', function(event){
-	let newTaskValue = document.getElementById('newTaskName').value;
-	let newTaskHour = document.getElementById('taskTimeHours').value;
-	let newTimeMinutes = document.getElementById('taskTimeMinutes').value;
-	let newTaskObject = new Task (newTaskValue,newTaskHour,newTimeMinutes);
-	dayTasks.push(newTaskObject);
-	clearNewTask();
-	if (newTaskValue) {
-		$('#userTasksList').fadeIn(1500);
-		$('#userTasksList ul').append(`<li>${newTaskObject.name} 
-			(${newTaskObject.hours} h ${newTaskObject.minutes} min)</li>`);
-	};
 
-	console.log(dayTasks);
-});
-
-//addin class .done when user click on the task
-$('#userTasksList').on('click', function(event){
-	target = event.target;
-	$(target).addClass('done');
-});
-
-//Calculation NONsleeping time in the day
- $('#sleepingTimeHours, #userSleepingTimeMinutes').on('change', function(){
- 	console.log('Change');
- });
- //draw free time in the day
- function drawFreeTime(){
- 	
- };
 
