7939298364bfbdb3e37201b39fa14eb6876c18eb
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-11-15 08:29:05 +0200

    Added function drawCircle

--- scripts/planMyDay.js
+++ scripts/planMyDay.js
@@ -7,8 +7,6 @@ let btnAddTask = document.querySelector(".addTask"),
 	sleepingMinutes = document.querySelector(".sleepingMinutes"),
 	day = [];
 const minInDay = 24 * 60;
-let canvas = document.querySelector("canvas"),
-	ctx = canvas.getContext("2d");
 
 function Task (name, hours, minutes){
 	this.name = name,
@@ -68,13 +66,24 @@ function converToHoursAndMinutes (minutes){
 function countPaddingInTasks(hours, minutes){
 	return `${Math.floor(hours * 10 + minutes/10)}px`;
 }
+function drawCircle (ctx, x, y, color, size, fill){
+	ctx.arc(x,y, size, 0, Math.PI*2);
+	(fill) ? ctx.fill() : ctx.stroke();
+}
 function drawInCanvas(){
-	let minSide,
+	let canvas = document.querySelector("canvas");
+	if(canvas.getContext) {
+		ctx = canvas.getContext("2d");
+		let minSide,
 		width = canvas.width,
 		height = canvas.height;
-	(width < height) ? minSide = width : minSide = height;
-	console.log(`canvas.width = ${canvas.width}, canvas.height = ${canvas.height}`);
-	console.log(`minSide of canvas is: ${minSide}`);
+		(width < height) ? minSide = width : minSide = height;
+		console.log(`canvas.width = ${canvas.width}, canvas.height = ${canvas.height}`);
+		console.log(`minSide of canvas is: ${minSide}`);
+
+		drawCircle (ctx, 40, 60, null, 60, false);
+	}
+	else canvas.textContent = "Use modern browser's version";
 }
 function random(min,max) {
   var num = Math.floor(Math.random()*(max-min)) + min;
