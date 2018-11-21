ceb83103c810d348b02730070a20003cb3aa69c2
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-11-21 06:27:25 +0200

    Some fixing with drawing totalRect and sleepingRect

--- scripts/planMyDay.js
+++ scripts/planMyDay.js
@@ -5,12 +5,15 @@ let btnAddTask = document.querySelector(".addTask"),
 	taskList = document.querySelector(".taskList"),
 	sleepingHours = document.getElementById("sleepingHours"),
 	sleepingMinutes = document.querySelector(".sleepingMinutes"),
+	canvas = document.querySelector("canvas"),
+	ctx = canvas.getContext("2d"),
 	day = [];
 const minInDay = 24 * 60;
 
 function Task (name, hours, minutes){
 	this.name = name,
-	this.duration = [hours, minutes];
+	this.hours = hours, 
+	this.minutes = minutes,
 	this.color = `rgb(${random(0,255)}, ${random(0,255)}, ${random(0,255)})`;
 }
 function clearTask (){
@@ -27,19 +30,20 @@ function countTime(){
 	if (sleepingMinutes === 60 || sleepingMinutes === -5) 
 		correctMinutesChanging (sleepingMinutes);
 	if (minutesSleep) {
-		let freeTime = minInDay - minutesSleep;
-		setAvailableTime(freeTime);
+		let minutesFree = minInDay - minutesSleep;
+		setAvailableTime(minutesFree);
 		if (day.length>0) {//if is user's tasks
 			let minutesDaySum = 0;
 			for (var i = day.length - 1; i >= 0; i--) {
-				minutesDaySum += convertToMinutes(day[i].duration[0], day[i].duration[1]);
+				minutesDaySum += convertToMinutes(day[i].hours, day[i].minutes);
+				drawTasks(i);
 			}
-			freeTime -= minutesDaySum;
+			minutesFree -= minutesDaySum;
 		}
-		let freeTimeArray = converToHoursAndMinutes(freeTime);
-		let msg = `${freeTimeArray[0]} h ${freeTimeArray[1]} m`
+		let minutesFreeArray = converToHoursAndMinutes(minutesFree);
+		let msg = `${minutesFreeArray[0]} h ${minutesFreeArray[1]} m`;
 		document.querySelector(".dayShow h2 span").textContent = msg;
-		drawInCanvas(minutesSleep);
+		drawInCanvas(minutesSleep, ctx);
 	}	
 }
 function correctMinutesChanging (sleepingMinutes){
@@ -78,53 +82,46 @@ function converToHoursAndMinutes (minutes){
 	}
 	return false;
 }
-function setAvailableTime(freeTime){
-	console.log(`freeTime ${freeTime} minutes`);
-	let freeTimeArray = converToHoursAndMinutes(freeTime);
-	let h = freeTimeArray[0];
-	let m = freeTimeArray[1];
+function setAvailableTime(minutesFree){
+	console.log(`MinutesFree ${minutesFree} minutes`);
+	let minutesFreeArray = converToHoursAndMinutes(minutesFree);
+	let h = minutesFreeArray[0];
+	let m = minutesFreeArray[1];
 	document.getElementById("hoursNewTask").setAttribute("max", h);
 }
-function drawCircle (x, y, color, size, fill){
+function drawCircle (x, y, color, size, fill, ctx){
 	ctx.arc(x,y, size, 0, Math.PI*2);
 	(fill) ? ctx.fill() : ctx.stroke();
 }
-function drawInCanvas(minutesSleep){
-	let canvas = document.querySelector("canvas");
-	if(canvas.getContext) {
-		ctx = canvas.getContext("2d");
-		let width = canvas.width,
-			height = canvas.height,
-			totalRectXstart = width*4/5,
-			sleepRectYstart;
+function drawInCanvas(minutesSleep, ctx){
+	let width = canvas.width,
+		height = canvas.height,
+		totalRectXstart = width*4/5,
+		widthTotal = width - totalRectXstart;
+		sleepRectYstart,
+		heightSleep;
 
-		let sleepTimePart = minutesSleep/minInDay;
-		console.log(`sleepTimePart ${sleepTimePart}`);
+	let sleepTimePart = minutesSleep/minInDay;
+	console.log(`sleepTimePart ${sleepTimePart}`);
 
-		ctx.clearRect(0, 0, width, height);
-		ctx.fillStyle = "rgba(255, 255, 255, 1)";
-		ctx.fillRect(totalRectXstart, 0, width, height);
-		ctx.fill();
-		/* draw sleep time rect */
-		ctx.fillStyle = "gold";
-		sleepRectYstart = height-height*sleepTimePart;
-		ctx.fillRect(totalRectXstart, sleepRectYstart, width, height);
-		ctx.fill();
-		/* draw task's rect */
-		if (day.length>0) {
-			for (var i = day.length - 1; i >= 0; i--) {
-				let name = day[i].name;
-				let hours = day[i].duration[0];
-				let minutes = day[i].duration[1];
-				drawCircle (40*i*hours, 60*i*hours, null, 60*i, true);
-				console.log(`need draw new tasks NAME: ${day[i].name} ${day[i].duration[0]}h ${day[i].duration[1]} m`);
-				ctx.fillStyle = day[i].color;
-				ctx.fillRect = (totalRectXstart, 140, width, height - sleepRectYstart);
-				ctx.fill();
-			}
+	ctx.clearRect(0, 0, width, height);
+	ctx.strokeStyle = "purple";
+	ctx.strokeRect(totalRectXstart, 0, widthTotal, height);
+	/* draw sleep time rect */
+	ctx.fillStyle = "gold";
+	sleepRectYstart = height-height*sleepTimePart;
+	heightSleep = height - sleepRectYstart;
+	console.log(`ctx.fillRect ${ctx.fillRect}`);
+	ctx.fillRect(totalRectXstart, sleepRectYstart, widthTotal, heightSleep);
+	/* draw task's rect */
+	if (day.length>0) {
+		for (var i = day.length - 1; i >= 0; i--) {
+			//drawCircle (40*i*hours, 60*i*hours, null, 60*i, true);
+			console.log(`need draw new tasks NAME: ${day[i].name} ${day[i].hours}h ${day[i].minutes} m`);
+			ctx.fillStyle = day[i].color;
+			ctx.fillRect = (totalRectXstart, 140, widthTotal, heightSleep);
 		}
 	}
-	else canvas.textContent = "Use modern browser's version";
 }
 function random(min,max) {
   var num = Math.floor(Math.random()*(max-min)) + min;
@@ -142,18 +139,20 @@ Ball.prototype.draw = function() {
   ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
   ctx.fill();
 }
+function drawTasks (i){
+	console.log(`day[${i}]: ${day[i]}`);
+}
 btnAddTask.addEventListener('click', function(){
 	let name = document.getElementById('newTaskName').value,
 		hours = document.getElementById("hoursNewTask").value,
 		minutes = document.querySelector(".minutes").value;
-	console.log(`NAME: ${name} HOURS: ${hours} MINUTES ${minutes}`);
 	if (name) {
 		let newTaskObj = new Task(name, hours, minutes);
 		day.push(newTaskObj);
+		console.log(`NAME: ${name} HOURS: ${hours} MINUTES ${minutes}`);
 		console.log(`Added new task in array: ${day[day.length-1]}`);
 		clearTask();
 		countTime();
-		drawTaskInCanvas();
 	}
 }, false);
 
